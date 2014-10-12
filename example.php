3f12a36ef8e0a9da02a01942da98ffd1932d3474
Daniel Boorn <daniel.boorn@gmail.com>
2014-10-11 21:57:33 -0400

    Release commit. ACH vaulted charge still pending (release)

---
+++ example.php
@@ -0,0 +1,72 @@
+<?php
+
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(-1);
+
+require_once 'sage/api.php';
+
+$merchantId = '287183824687';
+$merchantKey = 'R6C5S2S1Q7O8';
+
+$customer = array(
+    'C_NAME'            => 'John Doe',
+    'C_ADDRESS'         => '123 Someplace Dr',
+    'C_CITY'            => 'Myrtle Beach',
+    'C_STATE'           => 'SC',
+    'C_ZIP'             => '29579',
+    'C_COUNTRY'         => 'USA',
+    'C_EMAIL'           => 'test@example.com',
+    'T_CUSTOMER_NUMBER' => rand(1001, 9999),
+);
+
+$sage = new \Sage\API($merchantId, $merchantKey);
+
+$testCreditCard = function () use ($sage, $customer) {
+    //example of vaulting credit card
+    $vaultId = $sage->addCustomer(array(
+        'CARDNUMBER'      => '4111111111111111',
+        'EXPIRATION_DATE' => '1016',
+    ), $sage::CREDIT);
+    var_dump($vaultId);
+
+    //charge credit card
+    $response = $sage->chargeCustomer($vaultId, array_merge($customer, array(
+        'T_AMT'      => rand(20, 90),
+        'T_ORDERNUM' => rand(500, 600),
+    )), $sage::CREDIT);
+    var_dump($response);
+};
+
+
+$testAch = function () use ($sage, $customer) {
+    //example of vaulting ach (check)
+    $vaultId = $sage->addCustomer(array(
+        'ROUTING_NUMBER' => '056008849',
+        'ACCOUNT_NUMBER' => '12345678901234',
+        'C_ACCT_TYPE'    => $sage::ACH_CHECKING,
+    ), $sage::ACH);
+    var_dump($vaultId);
+
+//charge ach
+    $achCustomer = array_merge($customer, array(
+        'C_FIRST_NAME' => 'John',
+        'C_LAST_NAME'  => 'Doe',
+    ));
+    unset($achCustomer['C_NAME']);
+
+    $response = $sage->chargeCustomer($vaultId, array_merge($achCustomer, array(
+        'T_AMT'      => rand(20, 90),
+        'T_ORDERNUM' => rand(500, 600),
+    )), $sage::ACH);
+    var_dump($response);
+};
+
+
+// Uncomment below to test
+$testCreditCard();
+//$testAch();
+
+
+
---
+++ sage/api.php
@@ -0,0 +1,223 @@
+<?php
+
+
+/**
+ * Sage Soap API
+ * @author Daniel Boorn - daniel.boorn@gmail.com
+ * @copyright Daniel Boorn
+ * @license Apache
+ * @namespace Sage
+ */
+
+
+namespace Sage;
+
+/**
+ * Class Exception
+ * @package Sage
+ */
+class Exception extends \Exception
+{
+
+    /**
+     * @var null
+     */
+    public $response;
+    /**
+     * @var null
+     */
+    public $request;
+    /**
+     * @var bool
+     */
+    public $debug = false;
+
+    /**
+     * @param string $message
+     * @param int $code
+     * @param null $response
+     * @param null $request
+     * @param \Exception $previous
+     */
+    public function __construct($message, $code = 0, $response = null, $request = null, \Exception $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+        $this->response = $response;
+        $this->request = $request;
+    }
+
+    /**
+     * @return null
+     */
+    public function getResponse()
+    {
+        return $this->response;
+    }
+
+    /**
+     * @return null
+     */
+    public function getRequest()
+    {
+        return $this->request;
+    }
+
+}
+
+/**
+ * Class API
+ * @package Sage
+ */
+class API
+{
+    /**
+     *
+     */
+    const VAULT_URL = 'https://gateway.sagepayments.net/web_services/wsVault/wsVault.asmx?WSDL';
+    /**
+     *
+     */
+    const CREDIT_URL = 'https://gateway.sagepayments.net/web_services/wsVault/wsVaultBankcard.asmx?WSDL';
+    /**
+     *
+     */
+    const ACH_URL = 'https://gateway.sagepayments.net/web_services/wsVault/wsVaultVirtualCheck.asmx?WSDL';
+
+    /**
+     *
+     */
+    const CREDIT = 'creditcard';
+    /**
+     *
+     */
+    const ACH = 'ach';
+
+    /**
+     *
+     */
+    const ACH_CHECKING = 'DDA';
+    /**
+     *
+     */
+    const ACH_SAVINGS = 'SAV';
+
+    /**
+     *
+     */
+    const DECLINED = 'DECLINED';
+    /**
+     *
+     */
+    const UNABLE_TO_PROCESS = 'UNABLE TO PROCESS';
+    /**
+     *
+     */
+    const ERROR = 'ERROR';
+
+    /**
+     * @var
+     */
+    protected $soap;
+    /**
+     * @var
+     */
+    protected $merchantId;
+    /**
+     * @var
+     */
+    protected $merchantKey;
+    /**
+     * @var
+     */
+    protected $applicationId;
+
+    /**
+     * @param $merchantId
+     * @param $merchantKey
+     */
+    public function __construct($merchantId, $merchantKey)
+    {
+        $this->merchantId = $merchantId;
+        $this->merchantKey = $merchantKey;
+        $this->setSoap(self::VAULT_URL);
+    }
+
+    /**
+     * @param $url
+     */
+    protected function setSoap($url)
+    {
+        $this->soap = new \SoapClient($url, array('trace' => 1));
+    }
+
+    /**
+     * @param $merchantId
+     * @param $merchantKey
+     * @return API
+     */
+    public static function forge($merchantId, $merchantKey)
+    {
+        return new self($merchantId, $merchantKey);
+    }
+
+    /**
+     * @param array $params
+     * @param $paymentType
+     * @return mixed
+     */
+    public function addCustomer(array $params, $paymentType)
+    {
+        $this->setSoap(self::VAULT_URL);
+        $endpoint = $paymentType == self::CREDIT ? 'INSERT_CREDIT_CARD_DATA' : 'INSERT_VIRTUAL_CHECK_DATA';
+        $response = $this->fetch($params, $endpoint);
+        return current($response->GUID);
+    }
+
+    /**
+     * @param $id
+     * @param array $params
+     * @param $paymentType
+     * @return mixed
+     */
+    public function chargeCustomer($id, array $params, $paymentType)
+    {
+        $this->setSoap($paymentType == self::CREDIT ? self::CREDIT_URL : self::ACH_URL);
+        $endpoint = $paymentType == self::CREDIT ? 'VAULT_BANKCARD_SALE' : 'VIRTUAL_CHECK_WEB_SALE';
+        $params['GUID'] = $id;
+        return $this->fetch($params, $endpoint);
+    }
+
+    /**
+     * @param array $params
+     * @param $endpoint
+     * @return mixed
+     * @throws Exception
+     */
+    public function fetch(array $params, $endpoint)
+    {
+        $params['M_ID'] = $this->merchantId;
+        $params['M_KEY'] = $this->merchantKey;
+        $response = $this->soap->{$endpoint}($params);
+
+        if ($this->debug) {
+            var_dump( //example of how to debug soap
+                $params,
+                $endpoint,
+                $this->soap->__getLastRequestHeaders(),
+                $this->soap->__getLastRequest(),
+                $this->soap->__getLastResponse(),
+                $this->soap->__getLastResponseHeaders()
+            );
+        }
+
+        $result = new \SimpleXMLElement($response->{"{$endpoint}Result"}->any);
+        if (!empty($result->NewDataSet->Table1->SUCCESS) && $result->NewDataSet->Table1->SUCCESS != 'true') {
+            throw new Exception($result->NewDataSet->Table1->MESSAGE);
+        }
+        if (strpos($result->NewDataSet->Table1, self::DECLINED) !== false || strpos($result->NewDataSet->Table1, self::UNABLE_TO_PROCESS) !== false || strpos($result->NewDataSet->Table1, self::ERROR) !== false) {
+            throw new Exception(trim($result->NewDataSet->Table1->MESSAGE));
+        }
+        return $result->NewDataSet->Table1;
+    }
+
+}
